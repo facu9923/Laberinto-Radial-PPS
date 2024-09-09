6f3a2ce696608e64fc0a545cf8f3fa39ab71ef05
facu9923 <facundoarias1231@gmail.com>
2024-09-09 15:48:56 -0300

    cambios en decoraciones

--- Radial_Labyrinth_Game/Assets/Scripts/Objetivo.cs
+++ Radial_Labyrinth_Game/Assets/Scripts/Objetivo.cs
@@ -16,29 +16,9 @@ public class Objetivo : MonoBehaviour
             canvas.SetActive(false);
         }
 
-        int randomNumber = Random.Range(0, 8);
+        int randomNumber = Random.Range(0, 6);
         Debug.Log(randomNumber);
         transform.position = referencies[randomNumber].transform.position;
-        // positions[0] = new Vector3(36.651f, 7.42f, 10.03f);
-        
-        // if (randomNumber < 0.125){
-        //     Vector3 newPosition = new Vector3(36.651f, 7.42f, 10.03f);
-        //     transform.position = newPosition;
-        // }else if (randomNumber < 0.250)
-        // {
-        //     Vector3 newPosition = new Vector3(49.09f, 7.42f, 37.63f);
-        //     transform.position = newPosition;
-        // }
-        // else if (randomNumber < 0.375)
-        // {
-        //     Vector3 newPosition = new Vector3(6f, 7.04f, -2f);
-        //     transform.position = newPosition;
-        // }
-        // else if (randomNumber < 0.5)
-        // {
-        //     Vector3 newPosition = new Vector3(39f, 7.42f, 64.3f);
-        //     transform.position = newPosition;
-        // }
 
     }
 
